c621e031761755cb9da383bb60475c21ede0c34e
DenScobar <gdv201099@gmail.com>
2022-09-25 17:57:16 +0300

    Доделал все функции

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const calculatorContainer = document.querySelector('#calc-cont1')
 
 
-
 createCalculator(300, 500, calculatorContainer)
 // createCalculator(150, 250, calculatorContainer)
 // createCalculator(75, 125, calculatorContainer)
@@ -10,78 +9,97 @@ function createCalculator(width, height, container) {
     const buttons = [
         {
             dataValue: 'AC',
+            dataOption: 'func',
             className: 'key color1',
         },
         {
             dataValue: '+/-',
+            dataOption: 'func',
             className: 'key color1',
         },
         {
             dataValue: '%',
+            dataOption: 'func',
             className: 'key color1',
         },
         {
             dataValue: '/',
+            dataOption: 'func',
             className: 'key color2',
         },
         {
             dataValue: '7',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '8',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '9',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: 'x',
+            dataOption: 'func',
             className: 'key color2',
         },
         {
             dataValue: '4',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '5',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '6',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '-',
+            dataOption: 'func',
             className: 'key color2',
         },
         {
             dataValue: '1',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '2',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '3',
+            dataOption: 'num',
             className: 'key',
         },
         {
             dataValue: '+',
+            dataOption: 'func',
             className: 'key color2',
         },
         {
             dataValue: '0',
+            dataOption: 'num',
             className: 'key key2',
         },
         {
             dataValue: '.',
+            dataOption: 'func',
             className: 'key',
         },
         {
             dataValue: '=',
+            dataOption: 'func',
             className: 'key color2',
         },
 
@@ -107,67 +125,86 @@ function createCalculator(width, height, container) {
         first: '',
         second: '',
         total: '',
-        isTotal: false,
+        isPoint: false,
         operation: '',
         count: 0
     }
 
     buttons.forEach(button => {
-        const {dataValue, className} = button
+        const {dataValue, dataOption, className} = button
         const buttonCalc = document.createElement('div')
         const spanCalc = document.createElement('span')
         buttonCalc.classList.add(...className.split(' '))
         buttonCalc.dataset.value = dataValue
+        buttonCalc.dataset.option = dataOption
         buttonCalc.append(spanCalc)
         spanCalc.innerText = dataValue
         calculatorKeyboard.append(buttonCalc)
         buttonCalc.addEventListener('click', clickHandler)
     })
 
-
     function clickHandler(e) {
         const button = e.target.closest(`div`)
         const action = button.dataset.value
 
         const classes = button.className.split(' ')
-        if (classes.includes('color1') || classes.includes('color2')) {
+        if (button.dataset.option === 'func') {
             if (action === '=') {
-                calcObject.total = calculate(calcObject.first,calcObject.second,calcObject.operation)
+                calcObject.total = calculate(calcObject.first, calcObject.second, calcObject.operation)
                 calculatorScreen.querySelector('span').innerHTML = calcObject.total
                 calcObject.first = calcObject.total
+                calcObject.second = ''
                 calcObject.count = 0
+                calcObject.isPoint = false
+            } else if (action == 'AC') {
+                calcObject.first = ''
+                calcObject.second = ''
+                calcObject.operation = ''
+                calcObject.total = ''
+                calcObject.count = 0
+                calcObject.isPoint = false
+                calculatorScreen.querySelector('span').innerHTML = ''
+            } else if (action == '+/-') {
+                if (calcObject.second) {
+                    calcObject.second = -Number(calcObject.second)
+                    calculatorScreen.querySelector('span').innerHTML = calcObject.second
+                } else if (calcObject.first) {
+                    calcObject.first = -Number(calcObject.first)
+                    calculatorScreen.querySelector('span').innerHTML = calcObject.first
+                }
+
+            } else if(action == '.'){
+                if (calcObject.second && !calcObject.isPoint) {
+                    calcObject.second = (calcObject.second + '.')
+                    calculatorScreen.querySelector('span').innerHTML = calcObject.second
+                } else if (calcObject.first && !calcObject.isPoint) {
+                    calcObject.first = (calcObject.first + '.')
+                    calculatorScreen.querySelector('span').innerHTML = calcObject.first
+                }
+                calcObject.isPoint = true
             }
-            // if (action === 'AC'){
-            //     calculatorScreen.querySelector('span').innerHTML = ''
-            //     calcObject.second = ''
-            //     calcObject.first = ''
-            //     calcObject.count = 0
-            // }
-            calcObject.operation = action
-            calcObject.count = 1
+            else {
+                calcObject.operation = action
+                calcObject.count = 1
+                calcObject.isPoint = false
+            }
         }
-        else{
-            if(calcObject.count === 0) {
+        if(button.dataset.option === 'num') {
+            if (calcObject.count === 0) {
                 calcObject.first += action
                 calculatorScreen.querySelector('span').innerHTML = calcObject.first
-            }
-            else if(calcObject.count === 1) {
+            } else if (calcObject.count === 1) {
                 calcObject.second += action
                 calculatorScreen.querySelector('span').innerHTML = calcObject.second
             }
         }
-        console.log(calcObject)
     }
 
-
-    function calculate(first,second,operation){
+    function calculate(first, second, operation) {
         first = Number(first)
         second = Number(second)
         let answer = 0
         switch (operation) {
-            case '+/-':
-                answer = -first
-                break
             case '%':
                 answer = first % second
                 break
@@ -183,8 +220,10 @@ function createCalculator(width, height, container) {
             case '+':
                 answer = first + second
                 break
+            default:
+                answer = first || second
         }
-        return answer
+        return Math.trunc( answer * 1e4 ) / 1e4
     }
 }
 
